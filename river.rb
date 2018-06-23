8261112157a152fd538444f06ca5f9adc2e6bb94
cassidytrinh <cassidytrinh@gmail.com>
2018-06-23 17:44:24 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -42,7 +42,7 @@ end
 nile = River.new("Nile")
 nile.length = 4258
 nile.countries = ["Tanzania", "Uganda", "Rwanda", "Burundi", "Democratic Republic of the Congo", "Kenya", "Ethiopia", "Eritrea", "South Sudan", "Sudan", "Egypt"]
-nile.discharge =
+nile.discharge = 99940
 mississippi = River.new("Mississippi")
 mississippi.length =
 mississippi.countries =
@@ -50,15 +50,15 @@ mississippi.discharge =
 amazon = River.new("Amazon")
 amazon.length =
 amazon.countries =
-amazon.discharge = 
+amazon.discharge =
 amazon.flood
 seine = River.new("Seine")
 seine.length =
 seine.countries =
-seine.discharge = 
+seine.discharge =
 yangtze = River.new("Yangtze")
-yangtze.length = 
-yangtze.countries = 
+yangtze.length =
+yangtze.countries =
 yangtze.discharge =
 yangtze.flood
 euphrates = River.new("Euphrates")
